a11b9752a69692152ff15240802722b718d428c3
Brian Lynch Wade <brianlynchwade@gmail.com>
2022-08-28 17:56:36 +0200

    first commit

---
+++ wrangle-data.r
@@ -0,0 +1,5 @@
+library(tidyverse)
+murders <- read.csv('data/murders.csv')
+head(murders)
+murders <- murders %>% mutate(region = factor(region), rate = total/population * 10^5)
+save(murders, file = 'rda/murders.rda')
